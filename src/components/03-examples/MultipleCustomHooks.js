224cc702ecb61b3f73ad0e64d51f623828039796
Gabriel Bracamonte <gabobrako@gmail.com>
2021-12-10 01:37:41 -0500

    using 2 custom hooks to make an app

--- src/components/03-examples/MultipleCustomHooks.js
+++ src/components/03-examples/MultipleCustomHooks.js
@@ -1,14 +1,48 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useFetch } from '../../hooks/useFetch';
+import {useCounter} from '../../hooks/useCounter';
 import '../02-useEffect/effects.css';
 
 export const MultipleCustomHooks = () => {
-    const state = useFetch(`https://www.breakingbadapi.com/api/quotes/1`);
-    console.log(state);
+    const {state:counter, increment} = useCounter(1);
+    const {loading, data} = useFetch(`https://www.breakingbadapi.com/api/quotes/${counter}`);
+    const {quote, author} = (!!data && data[0]) || (!!data && {quote:'', author:''}); 
+
+    useEffect(() => {
+        quote === '' && increment();
+    }, [quote, increment]);
+    
+    
     return (
         <div>
-            <h1>Custom Hooks!!</h1>
+            <h1>BrakingBad Quotes</h1>
             <hr />
+
+            {
+                loading 
+                ?
+                    (
+                        <div className='alert alert-info text-center'>
+                            Loading...
+                        </div> 
+                    )
+                :
+                    (
+                        <blockquote className='blockquote text-end'>
+                            <p className='mb-3'>{quote}</p>
+                            <footer className='blockquote-footer'>{author}</footer>
+                        </blockquote>
+                    )
+            }
+
+            <button 
+                className='btn btn-primary' 
+                onClick={() => increment()}
+                disabled={loading}
+            >
+                Next Quote
+            </button>
+
         </div>
     )
 }
